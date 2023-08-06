35592eae72fab88aa8eb9411678afc9dd2dc78fe
whitecoldd <buyukliwc@gmail.com>
2023-08-06 23:33:24 +0300

    whitebang

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,95 +1,102 @@
 <script>
-	import forestBg from '$lib/assets/forest-bg.webp';
-	import forestLeft from '$lib/assets/forest-left.webp';
-	import forestRight from '$lib/assets/forest-right.webp';
+	import { browser } from '$app/environment';
+	import cityBg from '$lib/assets/nightcitybg.webp';
 	import chevy from '$lib/assets/chevron-down.svg';
-	import { onMount } from 'svelte';
-	import SplitType from 'split-type';
-	import { timeline, stagger } from 'motion';
 
-	let text;
+	import { timeline, stagger, spring, animate } from 'motion';
+	import { onMount } from 'svelte';
+
+	let text2;
 	let bg;
 	let left;
 	let right;
 	let btn;
-	let headText;
+	let bang;
 	let blocker;
-
+	if (browser) {
+		setInterval(() => {
+			animate(
+				text2,
+				{
+					color: ['#fff', '#000'],
+					filter: ['drop-shadow(0px 0px 0px #FFA500)', 'none']
+				},
+				{
+					duration: 0.2,
+					repeat: 2
+				}
+			);
+		}, 12000);
+		setInterval(() => {
+			const btnAnimateUp = [btn, { y: ['0%', '-20%'] }, { duration: 0.3, delay: stagger(3) }];
+			const btnAnimateDown = [btn, { y: ['-20%', '0%'] }, { duration: 0.1, easing: spring() }];
+			timeline([btnAnimateUp, btnAnimateDown]);
+		}, 1000);
+	}
 	onMount(() => {
-		const splitText = new SplitType(text, { lineClass: 'overflow-hidden -my-14' });
-		const splitHeadText = new SplitType(headText, { lineClass: 'overflow-hidden' });
-		const bgAnimate = [bg, { scale: [1.15, 1] }, { duration: 2, at: '<' }];
-		const leftAnimate = [left, { x: ['-100%', '0%'] }, { duration: 2, at: '<' }];
-		const rightAnimate = [right, { x: ['100%', '0%'] }, { duration: 2, at: '<' }];
-		const btnAnimate = [btn, { y: ['100%', '0%'] }, { duration: 1, at: '-1' }];
-		const animateText = [
-			splitText.words,
-			{ y: ['100%', '0%'] },
-			{ delay: stagger(0.1), duration: 1, easing: [0.62, -0.01, 0.37, 0.99], at: '-0.7' }
+		const animateLeft = [left, { x: ['-1000px', '0px'] }, { duration: 1.5, easing: 'ease' }];
+		const animateRight = [
+			right,
+			{ x: ['1000px', '0px'] },
+			{ duration: 1.5, easing: 'ease', at: '<' }
 		];
-		const revealHeadText = [headText, { opacity: 1 }, { duration: 0 }];
-		const animateHeadTextIn = [
-			splitHeadText.words,
-			{ y: ['100%', '0%'] },
-			{ delay: stagger(0.1), duration: 1 }
+		const animateBg = [bg, { scale: [1.15, 1] }, { duration: 2, at: '+0.7' }];
+		const animateBlocker = [blocker, { opacity: [1, 0] }, { duration: 1 }];
+		const animateBang1 = [
+			bang,
+			{ width: ['0%', '150%'] },
+			{ duration: 1, easing: [0.17, 0.67, 0.83, 0.67] }
 		];
-		const animateHeadTextOut = [
-			splitHeadText.words,
-			{ y: ['-100%'] },
-			{ delay: stagger(0.1), duration: 1, at: '+0.1' }
+		const animateBang2 = [
+			bang,
+			{ height: ['0%', '250%'] },
+			{ duration: 1, easing: [0.17, 0.67, 0.83, 0.67], at: '<' }
 		];
-		const animateBlocker = [blocker, { y: ['-100%'] }, { duration: 1, at: '-0.35' }];
-		timeline(
-			[
-				revealHeadText,
-				animateHeadTextIn,
-				animateHeadTextOut,
-				animateBlocker,
-				animateText,
-				bgAnimate,
-				leftAnimate,
-				rightAnimate,
-				btnAnimate
-			],
-			{
-				defaultOptions: { ease: [0.52, 0.05, 0.22, 0.95] }
-			}
-		);
+		timeline([animateLeft, animateRight, animateBang1, animateBang2, animateBlocker, animateBg]);
 	});
 </script>
 
 <div
 	class="h-screen w-full flex items-center justify-center bg-primary-400 relative overflow-hidden"
 >
-	<h1 bind:this={text} class="font-medieval text-huge text-center text-light-100 z-20">
-		Welcome <br /> to the <br /> jungle
-	</h1>
+	<div class="py-1 px-3 z-20">
+		<h1 class="font-neon text-huge text-center text-light-100 z-20">
+			<span class="text-white drop-shadow-neon">white</span>
+			<span class="text-white drop-shadow-flame">cold</span><span
+				bind:this={text2}
+				class="text-white drop-shadow-flame">d</span
+			>
+		</h1>
+	</div>
 	<div class="absolute w-full h-full">
 		<div class="h-full w-full absolute bg-fade" />
-		<img bind:this={bg} src={forestBg} alt="forest" class="blur-sm h-full w-full object-cover" />
+		<img bind:this={bg} src={cityBg} alt="forest" class="blur-sm h-screen w-full object-cover" />
 	</div>
-	<div class="absolute inset-0 overflow-hidden h-full w-2/5 flex items-center z-30 blur">
+	<!-- <div class="absolute top-0 left-0 overflow-hidden h-full w-2/5 flex items-center z-30">
 		<img bind:this={left} src={forestLeft} alt="left" class="w-full min-h-full object-cover" />
 	</div>
 	<div class="absolute top-0 right-0 overflow-hidden h-full w-2/5 flex items-center z-30 blur">
 		<img bind:this={right} src={forestRight} alt="right" class="w-full min-h-full object-cover" />
-	</div>
-	<button bind:this={btn} class="absolute -bottom-14 rounded-full p-4 border border-light-100">
-		<div class="rounded-full px-4 py-10 bg-brown-300 flex items-center justify-center">
-			<img src={chevy} alt="chevy" class="w-8 h-8" />
+	</div> -->
+	<button bind:this={btn} class="absolute -bottom-2">
+		<div class="px-4 py-4 bg-flame-200 drop-shadow-flame flex items-center justify-center">
+			<img src={chevy} alt="chevy" class="w-8 h-8 drop-shadow-white" />
 		</div>
 	</button>
-	<div class="z-50 fixed h-screen w-screen">
+	<div class="z-40 fixed h-screen w-screen">
 		<div
 			bind:this={blocker}
-			class="absolute bg-primary-400 w-full h-full flex items-center justify-center"
+			class="absolute bg-black w-full h-full flex items-center justify-center"
 		>
-			<span
-				bind:this={headText}
-				class="font-medieval text-center text-3xl text-light-100 relative opacity-0"
-			>
-				maybe next time
-			</span>
+			<div
+				bind:this={left}
+				class="rounded-full w-3 h-3 bg-flame-100 translate-x-1/2 drop-shadow-flameobj blur-sm"
+			/>
+			<div
+				bind:this={right}
+				class="rounded-full w-3 h-3 bg-neon-100 -translate-x-1/2 drop-shadow-neonobj blur-sm"
+			/>
+			<div bind:this={bang} class="bg-white rounded-full fixed z-50 h-0 w-0" />
 		</div>
 	</div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,16 +3,26 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
-			fontFamily: { medieval: ['MedievalSharp', 'cursive'] },
+			fontFamily: { medieval: ['MedievalSharp', 'cursive'], neon: ['Neon Tubes'] },
 			fontSize: { huge: ['14.75rem', '14.75rem'] },
 			colors: {
 				light: { 100: '#E8DEBF', 300: '#C7AC68' },
 				brown: { 300: '#AA6132', 400: '#7E431E' },
-				primary: { 100: '#92B17A', 200: '#7B9B65', 300: '#285125', 400: '#1B341E', 500: '#0A140A' }
+				primary: { 100: '#92B17A', 200: '#7B9B65', 300: '#285125', 400: '#1B341E', 500: '#0A140A' },
+				neon: { 100: '#00FFFF', 200: '#00B8D4', 300: '#29B6F6' },
+				flame: { 100: '#FFA500', 200: '#FF5722', 300: '#FF4500' }
 			},
 			backgroundImage: {
 				fade: 'background: radial-gradient(50% 50.00% at 50% 50.00%, rgba(0, 0, 0, 0.00) 0%, rgba(27, 52, 30, 0.50) 100%), linear-gradient(90deg, rgba(10, 20, 10, 0.90) 0%, rgba(10, 20, 10, 0.00) 21.88%, rgba(10, 20, 10, 0.00) 79.17%, rgba(10, 20, 10, 0.90) 92.86%);'
-			}
+			},
+			dropShadow: {
+				neon: '0px 0px 7px #00FFFF',
+				neonobj: '0px 0px 10px #00FFFF',
+				flame: '0px 0px 7px #FFA500',
+				flameobj: '0px 0px 20px #FFA500',
+				white: '0px 0px 7px #FFA500'
+			},
+	
 		}
 	},
 	plugins: []
